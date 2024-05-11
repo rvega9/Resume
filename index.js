6ad8f0607c59e5d2c3cb96d8d978ed1d57048ca6
wingklevega16@gmail.com <wingklevega16@gmail.com>
2024-05-11 13:06:24 +0800

    many updates

--- index.js
+++ index.js
@@ -12,33 +12,38 @@ const resumeData = {
     lastName: "Vega",
     jobTitle: "Student",
     contactNo: "09231418471",
-    website: "www.examplesite.com",
+    website: "www.rvegasite.com",
     location: "Cebu-PH",
-    aboutMe: "I am a 2nd year student taking Bachelor of Science in Information Technology",
+    aboutMe: "I am a 2nd year student taking Bachelor of Science in Infromation Technology, here in University of Southern Philippines (Lahug Campus) but at the same time, a working student as a Web Developer in Cebu-PH. I love to be around with people who have the same humor as me. I am a competent person and knows how to manage my time properly.",
+  },
+
+  leftInfo: {
     age: "24",
     birthDate: "September 14, 1999",
     address: "#47 Cashra, Lahug, Cebu City, Cebu",
     email: "wingklevega16@gmail.com",
+    status: "Single",
+    religion: "Roman Catholic"
   },
 
   languages: [
     { language: "English" },
     { language: "Tagalog" },
-    { language: "Bisaya" },
+    { language: "Cebuano" },
   ],
   
   skills: [
-    { description: "Web Development", expertiseLevel: "Advanced" },
+    { description: "Web Developer", expertiseLevel: "Advanced" },
     { description: "Software Engineer", expertiseLevel: "Advanced" },
   ],
   workExperience: [
-    { companyName: "Accenture",  details: "Developed web applications", year: "2016-2018" },
-    { companyName: "Accenture",  details: "Developed computer games", year: "2018-2022" },
+    { companyName: "Accenture",  details: "Developed Web Applications", year: "2016-2018" },
+    { companyName: "Cebu-PH",  details: "Developed Computer Games", year: "2018-2022" },
   ],
   education: [
-    { school: "Danao City Central School", year: "2011-2012"},
-    { school: "University of Southern Philippines Foundation", year: "2015-2016"},
-    { school: "Silliman University", year: "2017-2018"},
+    { school: "Danao City Central School", year: "Elementary, 2011-2012"},
+    { school: "University of Southern Philippines Foundation", year: "Junior High School, 2015-2016"},
+    { school: "Silliman University", year: "Senior High School, 2017-2018"},
     { school: "University of Southern Philippines Foundation", year: "Present"},
   ],
 
